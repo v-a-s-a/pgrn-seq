c7eacf04f68cb475d1942f8e246ec2b307ed67e7
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2014-08-27 11:15:53 -0500

    switched out kable (wasn't formatting right) to xtable (it works)

--- analyses/shared_variables.R
+++ analyses/shared_variables.R
@@ -7,7 +7,12 @@ PHENO.FN <- '../data/rdata/pheno.Rdata'
 ### FULL: all samples included
 ### EURO: only samples in european cluster of PC plot
 EURO.EXOME.GENO.FN <- '../data/rdata/euro.exome.geno.Rdata'
-FULL.EXOME.GENO.FN <- '../data/rdata/euro.exome.geno.Rdata'
+FULL.EXOME.GENO.FN <- '../data/rdata/full.exome.geno.Rdata'
 FULL.SEQ.GENO.FN <- '../data/rdata/full.seq.geno.Rdata'
 EURO.SEQ.GENO.FN <- '../data/rdata/euro.seq.geno.Rdata'
+
+## Models
+basic.model <- trans.fun(ANC) ~ 1
+full.model <- trans.fun(ANC) ~ sex + site + as.numeric(dose)
+pca.model <- trans.fun(ANC) ~ sex + site + as.numeric(dose) + eigenvect.1 + eigenvect.2 + eigenvect.3 + eigenvect.4 + eigenvect.5
 
