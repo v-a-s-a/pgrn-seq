7eac49b69b77367216a1148fca1439ab6b140a94
Vasa Trubetskoy <trubetskoy.vasa@gmail.com>
2014-08-27 09:37:01 -0500

    factoring out shared variables into separate file

---
+++ analyses/shared_variables.R
@@ -0,0 +1,13 @@
+### Keep shared variables in one place
+
+## Dataframe of clean phenotype data
+PHENO.FN <- '../data/rdata/pheno.Rdata'
+
+## GenABEL genotype data objects
+### FULL: all samples included
+### EURO: only samples in european cluster of PC plot
+EURO.EXOME.GENO.FN <- '../data/rdata/euro.exome.geno.Rdata'
+FULL.EXOME.GENO.FN <- '../data/rdata/euro.exome.geno.Rdata'
+FULL.SEQ.GENO.FN <- '../data/rdata/full.seq.geno.Rdata'
+EURO.SEQ.GENO.FN <- '../data/rdata/euro.seq.geno.Rdata'
+
